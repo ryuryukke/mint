d6fbc062977b5616d537e9d41af51f94f9613344
ryuryukke <ryuto-alexander-koike@keio.jp>
2025-10-19 17:11:01 +0900

    init

---
+++ data/Pile/collect_prefix.sh
@@ -0,0 +1,7 @@
+# load the pile dataset
+python collect_pile.py
+
+# collect prefixes for a specific domain
+for DOMAIN in wikipedia_\(en\) github pile_cc pubmed_central arxiv dm_mathematics hackernews; do
+python collect_prefix.py --domain $DOMAIN --sample_size 10
+done
